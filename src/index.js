dd02905be09950bbc14d45b7b8b142f04bb24d53
acuraintegurl <39477899+acuraintegurl@users.noreply.github.com>
2021-11-22 22:11:36 +1100

    added description

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 function displayTemperature(response) {
+  console.log(response);
   let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
 }
 
 let apiKey = "7a010bcd1946ade46c95e4c98b549354";
-let cityName = "New York";
+let cityName = "london";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
