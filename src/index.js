ffe1e68c468919e269fa747d463dac2f3cbc347b
acuraintegurl <39477899+acuraintegurl@users.noreply.github.com>
2021-12-07 20:04:31 +1100

    Fixed Date to Day

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ function formatDate(timestamp) {
     "Friday",
     "Saturday",
   ];
-  let day = days[date.getDate()];
+  let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
 
